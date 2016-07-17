4268da11f4d78f93a6d950b8f7ca70bf59178dda
Maxim <maxim.tis96@gmail.com>
2016-07-18 02:38:34 +0300

    Admin auth

--- RentACar/Controllers/AdminController.cs
+++ RentACar/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using RentACar.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace RentACar.Controllers
 {
@@ -12,7 +14,28 @@ namespace RentACar.Controllers
         // GET: Admin
         public ActionResult Index()
         {
-            return View(db.Orders.OrderByDescending(x=>x.OrderId).First());
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            if (model.Username != null && model.Password != null)
+            {
+                var log = model.Username;
+                var pass = model.Password;
+
+                if (System.Configuration.ConfigurationManager.AppSettings["AdminLogin"] == log &&
+                    System.Configuration.ConfigurationManager.AppSettings["AdminPassword"] == pass)
+                {
+                    FormsAuthentication.RedirectFromLoginPage(log, false);
+                }
+            }
+            return View();
+
+        }
+        public ActionResult AdminPanel()
+        {
+            return View(db.Orders.OrderByDescending(x => x.OrderId).First());
         }
     }
 }
