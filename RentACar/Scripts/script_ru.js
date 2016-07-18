c91f5887cdf36ffc42b675099769c1a3f0d834f7
urusa <urusa@DESKTOP-Q1VGNHG>
2016-07-18 13:27:50 +0300

    Скобки

--- RentACar/Scripts/script_ru.js
+++ RentACar/Scripts/script_ru.js
@@ -175,9 +175,8 @@ $(document).ready(function () {
                             tempArray = null;
                             tempArray = time2.split(':');
                             var endTime = tempArray[0];
-                            alert(endTime > startTime);
                             if (endTime > startTime)
-                            { newDate = newDate + 1; }
+                                newDate = newDate + 1;
                             var avg = newDate * price;
                             $('#avg').html(avg + "$");
                         } else {
